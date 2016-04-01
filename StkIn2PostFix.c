380da1b8cf57a455ec183b2550dfc67198456bbb
rg665n <rishabh.p.gandhi@gmail.com>
2016-04-01 09:07:37 +0530

    Fully Functional InfixToPostFix for +-*/^

--- StkIn2PostFix.c
+++ StkIn2PostFix.c
@@ -22,7 +22,7 @@ struct Stack* createStack(unsigned stk_max)
 
 //Check if input is operator
 int isOperator(char t) {
-    if (t=='+' || t=='-' || t=='*' || t=='/')
+    if (t=='+' || t=='-' || t=='*' || t=='/' || t=='^')
         return 1;
     return 0;
 }
@@ -35,11 +35,14 @@ int isParenthesis(char t) {
     return 0; //Not Parenthesis
 }
 
-int getPrecedence(char t) {
-    if (t=='*' || t=='/') //Higher Priority
-        return 1; //Priority 1
+int getPrecedence(char t) {     //Here 1 is lowest priority and number higher than 1 has more priority
+    if( t=='^') {
+        return 3; //Highest Priority
+    }
+    else if (t=='*' || t=='/' ) //Higher Priority
+        return 2; //Priority High
     else //(t=='+' || t=='-') //Lower Priority
-        return 2; //Priority 2
+        return 1; //Priority Low
 }
 
 // Stack is full when top is equal to the last index
@@ -60,7 +63,7 @@ void push(struct Stack* stack, char item)
     }
 
     stack->array[++stack->top] = item;
-    printf("%d pushed to stack\n", item);
+    printf("%c pushed to stack\n", item);
 }
 
 // Function to remove an item from stack.  It decreases top by 1
@@ -69,7 +72,7 @@ char pop(struct Stack* stack)
     if (isEmpty(stack)) {
       return CHAR_MIN;
     }
-
+    printf("%c poped out",stack->array[stack->top]);
     return stack->array[stack->top--];
 }
 
@@ -83,7 +86,7 @@ char peek(struct Stack* stack)
 
 void in2post (char *inexp) {
     char t;
-    char postExp[20];
+    char postExp[25];
     int i,j;
     struct Stack* stack;
     i=j=0;
@@ -92,36 +95,53 @@ void in2post (char *inexp) {
     while(inexp[i] != '\0') {
 
         if(isParenthesis(inexp[i]) == 2) {  //Right Parenthesis
-                printf("\n\n Right Parenthesis");
+                printf("\nRight Parenthesis");
+                while(peek(stack) != '(') { // pop until a left parenthesis encounters
+                    if(peek(stack) != CHAR_MIN){
+                       postExp[j] = pop(stack);
+                       j++;
+                    }
+                }
+                pop(stack); //Remove Left Parenthesis
         }
 
         else if(isParenthesis(inexp[i]) == 1 || isOperator(inexp[i]) == 1) {        //Operator or Left Parenthesis
             if(isParenthesis(inexp[i]) == 1 ) {
-                printf("\n\nLeft Parenthesis");
+                printf("\nLeft Parenthesis");
+                push(stack, inexp[i]);  // Push Left Parenthesis on Stack
             }
             if (isOperator(inexp[i]) == 1) {
-                printf("\n\nOperator");
+                printf("\nOperator");
+                while((getPrecedence(inexp[i]) <= getPrecedence(peek(stack))) && (peek(stack) != CHAR_MIN) && (isParenthesis(peek(stack)) != 1 )  ) {
+                   postExp[j] = pop(stack);
+                   j++;
+                }
+                push(stack, inexp[i]);
             }
         }
         else{  //if t is operand
             postExp[j] = inexp[i];
-            printf("\n\n%c",postExp[j]);
-            push(stack, inexp[i]);
+            printf("\n%c",postExp[j]);
             j++;
         }
         i++;
+
+    }
+    while(peek(stack) != CHAR_MIN) {
+        postExp[j] = pop(stack);
+        j++;
     }
     postExp[j] = '\0';
-    printf("\n\nOperand List : %s\n\n",postExp);
-    printf("%c popped from stack\n", pop(stack));
-    printf("%c popped from stack\n", pop(stack));
-    printf("%c popped from stack\n", pop(stack));
-    printf("%c popped from stack\n", pop(stack));
+    printf("\nOperand List : %s\n",postExp);
+    //printf("%c popped from stack\n", pop(stack));
+    //printf("%c popped from stack\n", pop(stack));
+    //printf("%c popped from stack\n", pop(stack));
+    //printf("%c popped from stack\n", pop(stack));
 }
 
 int main()
 {
-     char inexp[20];
+     char inexp[25];
      printf("Please Enter Infix Expression : ");
      scanf("%s",inexp);
      printf("\n\nInput Infix Expression is : ");
